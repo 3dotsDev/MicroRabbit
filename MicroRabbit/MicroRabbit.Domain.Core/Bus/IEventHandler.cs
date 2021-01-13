41b0f3fc11ad15d79c9788ded3396789cb642c09
dalmaso <idea@3dots.ch>
2021-01-13 21:06:52 +0100

    Implementation of RabbitMqBus completed

--- MicroRabbit/MicroRabbit.Domain.Core/Bus/IEventHandler.cs
+++ MicroRabbit/MicroRabbit.Domain.Core/Bus/IEventHandler.cs
@@ -3,8 +3,8 @@ using MicroRabbit.Domain.Core.Events;
 
 namespace MicroRabbit.Domain.Core.Bus
 {
-    public interface IEventHandler <in TEvent> : IEventHandler
-    where TEvent : Event
+    public interface IEventHandler<in TEvent> : IEventHandler
+        where TEvent : Event
     {
         Task Handle(TEvent @event);
     }
