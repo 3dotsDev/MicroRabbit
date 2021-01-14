9e755c496324dd603f4a1a2114fa70d41bd17a62
dalmaso <idea@3dots.ch>
2021-01-14 12:26:46 +0100

    Adding the Command for transfering Events to Rabbit Mq

---
+++ MicroRabbit/MicroRabbit.Banking.Domain/Commands/TransferCommand.cs
@@ -0,0 +1,11 @@
+using MicroRabbit.Domain.Core.Command;
+
+namespace MicroRabbit.Banking.Domain.Commands
+{
+    public abstract class TransferCommand : Command
+    {
+        public int To { get; protected set; }
+        public int From { get; protected set; }
+        public decimal Amount { get; protected set; }
+    }
+}
