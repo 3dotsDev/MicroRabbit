1760819be2e67486b6524427f3ea353948d5d4f5
dalmaso <idea@3dots.ch>
2021-01-14 10:56:08 +0100

    adding basic controller for account

--- MicroRabbit/MicroRabbit.Banking.Api/Controllers/BankingController.cs
+++ MicroRabbit/MicroRabbit.Banking.Api/Controllers/BankingController.cs
@@ -1,7 +1,26 @@
+using System.Collections.Generic;
+using MicroRabbit.Banking.Application.Interfaces;
+using MicroRabbit.Banking.Domain.Models;
+using Microsoft.AspNetCore.Mvc;
+
 namespace MicroRabbit.Banking.Api.Controllers
 {
-    public class BankingController
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BankingController : ControllerBase
     {
-        
+        private readonly IAccountService _accountService;
+
+        public BankingController(IAccountService accountService)
+        {
+            _accountService = accountService;
+        }
+
+        // Get api/banking
+        [HttpGet]
+        public ActionResult<IEnumerator<Account>> Get()
+        {
+            return Ok(_accountService.GetAccounts());
+        }
     }
 }
